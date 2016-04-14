e94bed483b9f13d9dbdf8e92d983736f75189932
Jhen <developer@jhen.me>
2016-04-15 02:39:25 +0800

    Remove unicode-regex
    6.0.0-rc.2 upgraded to v8 5.0

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 module.exports = {
   plugins: [
     require('babel-plugin-transform-es2015-modules-commonjs'),
-    require('babel-plugin-transform-es2015-unicode-regex'),
     require('babel-plugin-transform-es2015-function-name'),
   ],
 };
