8c2fc005aa1fd804062e31e4f7bde8636683b218
Jhen <developer@jhen.me>
2016-08-05 10:52:14 +0800

    Add `loose` option for preset

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-module.exports = {
-  plugins: [
-    require('babel-plugin-transform-es2015-modules-commonjs'),
-    require('babel-plugin-transform-es2015-function-name'),
-  ],
-};
+function preset(context, opts) {
+  var loose = opts && !!opts.loose;
+
+  return {
+    plugins: [
+      [require('babel-plugin-transform-es2015-modules-commonjs'), { loose: loose }],
+      require('babel-plugin-transform-es2015-function-name'),
+    ],
+  };
+}
+
+module.exports = preset({});
+
+Object.defineProperty(module.exports, "buildPreset", {
+  configurable: true,
+  writable: true,
+  enumerable: false,
+  value: preset,
+});
--- object-rest.js
+++ object-rest.js
@@ -1,9 +1,22 @@
 // https://phabricator.babeljs.io/T7086
-module.exports = {
-  plugins: [
-    require('babel-plugin-transform-es2015-destructuring'),
-    require('babel-plugin-transform-es2015-parameters'),
-    require('babel-plugin-transform-es2015-modules-commonjs'),
-    require('babel-plugin-transform-es2015-function-name'),
-  ],
-};
+function preset(context, opts) {
+  var loose = opts && !!opts.loose;
+
+  return {
+    plugins: [
+      [require('babel-plugin-transform-es2015-destructuring'), { loose: loose }],
+      require('babel-plugin-transform-es2015-parameters'),
+      [require('babel-plugin-transform-es2015-modules-commonjs'), { loose: loose }],
+      require('babel-plugin-transform-es2015-function-name'),
+    ],
+  };
+}
+
+module.exports = preset({});
+
+Object.defineProperty(module.exports, "buildPreset", {
+  configurable: true,
+  writable: true,
+  enumerable: false,
+  value: preset,
+});
